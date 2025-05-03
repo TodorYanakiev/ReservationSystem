326cf4dd78fadc8876790a2df93e3e3e976138a5
TodorYanakiev <todoryanakiev395@gmail.com>
2025-05-03 23:17:34 +0300

    Add CreateReservation method

--- BusinessLogic/Services/ReservationService.cs
+++ BusinessLogic/Services/ReservationService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLogic.Services.Email;
+using ReservationSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,58 @@ namespace BusinessLogic.Services
 {
     public class ReservationService
     {
+        private readonly RestaurantDbContext _context;
+
+        private readonly EmailService emailService = new EmailService(new SmtpSettings());
+
+        private static readonly Random _random = new Random();
+
+        private const string _chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        public ReservationService(RestaurantDbContext context)
+        {
+            _context = context;
+
+        }
+
+        public async void CreateReservation(Reservation reservation)
+        {
+            //TODOR fix email sending
+            CheckIfThereIsReservationWithTheSameDateAndTimeForTable(reservation);
+            reservation.VerificationCode = CreateVerificationCodeForReservation();
+            reservation.VerifiedByUser = false;
+            reservation.CreatedAt = DateTime.Now;
+            _context.Reservations.Add(reservation);
+            _context.SaveChanges();
+
+            emailService.SendEmailAsync(reservation.Email, "Verification code",
+                $"Your verification code is: {reservation.VerificationCode}");
+        }
+
+        private string CreateVerificationCodeForReservation()
+        {
+            char[] code = new char[8];
+
+            for (int i = 0; i < code.Length; i++)
+            {
+                code[i] = _chars[_random.Next(_chars.Length)];
+            }
+
+            return new string(code);
+
+        }
+
+        private void CheckIfThereIsReservationWithTheSameDateAndTimeForTable(Reservation reservationForCheck)
+        {
+            List<Reservation> reservationsForTable = _context.Reservations
+                .Where(re => re.TableId == reservationForCheck.TableId)
+                .Where(re => re.OperatingHoursId == reservationForCheck.OperatingHoursId)
+                .ToList();
+            foreach (Reservation reservation in reservationsForTable)
+            {
+                if (reservation.ReservationDate == reservationForCheck.ReservationDate)
+                    throw new ArgumentException("There is existing reservation for this date and time.");
+            }
+        }
     }
 }
--- PresentationLayerConsole/Program.cs
+++ PresentationLayerConsole/Program.cs
@@ -4,8 +4,17 @@ using System;
 
 class Programm
 {
-    public static async Task Main(String[] args)
+    public static void Main(String[] args)
     {
-        
+        ReservationService service = new ReservationService(new RestaurantDbContext());
+        Reservation reservation = new Reservation();
+        reservation.Name = "Toshko";
+        reservation.Email = "todormaster25@gmail.com";
+        reservation.PhoneNumber = "23462534";
+        reservation.TableId = 12;
+        reservation.OperatingHoursId = 67;
+        reservation.ReservationDate = new DateOnly(2025, 5, 22);
+
+        service.CreateReservation(reservation);
     }
 }
