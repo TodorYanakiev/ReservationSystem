b3a75a33f6fc86778e5fce645cdc51f29e0566ba
TodorYanakiev <todoryanakiev395@gmail.com>
2025-05-03 14:43:01 +0300

    Add SmtpSettings for EmailService

---
+++ BusinessLogic/Services/Email/SmtpSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace BusinessLogic.Services.Email
+{
+    public class SmtpSettings
+    {
+        public string Host { get; set; } = "smtp.gmail.com";
+        public int Port { get; set; } = 587;
+        public string Username { get; set; } = "todoryanakiev395@gmail.com";
+        public string Password { get; set; } = Environment.GetEnvironmentVariable("SMTP_PASSWORD");
+        public bool EnableSsl { get; set; } = true;
+        public string From { get; set; } = "todoryanakiev395@gmail.com";
+    }
+}
