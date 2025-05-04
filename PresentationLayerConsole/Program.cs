9454dfc48597ca4f5ebf5b340097eb5bad9b9e82
TodorYanakiev <todoryanakiev395@gmail.com>
2025-05-04 20:17:16 +0300

    Clean main method

--- PresentationLayerConsole/Program.cs
+++ PresentationLayerConsole/Program.cs
@@ -6,15 +6,5 @@ class Programm
 {
     public static void Main(String[] args)
     {
-        ReservationService service = new ReservationService(new RestaurantDbContext());
-        Reservation reservation = new Reservation();
-        reservation.Name = "Toshko";
-        reservation.Email = "todormaster25@gmail.com";
-        reservation.PhoneNumber = "23462534";
-        reservation.TableId = 12;
-        reservation.OperatingHoursId = 67;
-        reservation.ReservationDate = new DateOnly(2025, 5, 22);
-
-        service.CreateReservation(reservation);
     }
 }
