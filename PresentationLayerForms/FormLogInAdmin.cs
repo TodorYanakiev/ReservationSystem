c5e10e77185e9104a7fe1dfe8d2d9a8a4d59c2ed
София Станчева <sofi.stancheva@gmail.com>
2025-05-04 16:14:43 +0300

    LogInAdmin

--- PresentationLayerForms/FormLogInAdmin.cs
+++ PresentationLayerForms/FormLogInAdmin.cs
@@ -10,11 +10,33 @@ using System.Windows.Forms;
 
 namespace PresentationLayerForms
 {
-    public partial class FormLogInAdmin: Form
+    public partial class FormLogInAdmin : Form
     {
         public FormLogInAdmin()
         {
             InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdminLogIn_Click(object sender, EventArgs e)
+        {
+            FormAdmin formAdmin = new FormAdmin();
+            formAdmin.Show();
+            this.Hide();
+        }
+
+        private void textBoxUsernameAdmin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxPasswordAdmin_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
