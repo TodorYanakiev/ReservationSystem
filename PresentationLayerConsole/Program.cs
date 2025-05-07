e50a62587944012574e564557386835bcd1eeac3
TodorYanakiev <todoryanakiev395@gmail.com>
2025-05-08 02:35:52 +0300

    clean main

--- PresentationLayerConsole/Program.cs
+++ PresentationLayerConsole/Program.cs
@@ -7,11 +7,6 @@ class Programm
 {
     public static async Task Main(String[] args)
     {
-        ReservationService service = new ReservationService(new RestaurantDbContext());
-        List<Reservation> reservations = service.GetReservations(includeCancelled:true);
-        foreach (Reservation reservation in reservations)
-        {
-            Console.WriteLine($"{reservation.Id}");
-        }
+
     }
 }
